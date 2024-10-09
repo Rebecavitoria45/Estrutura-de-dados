13dec26a700d2202386b501a98dd17acb707e990
Rebecavitoria45 <rebecanascimentovit@gmail.com>
2024-10-09 09:42:50 -0300

    arrayList

---
+++ src/Lista.java
@@ -0,0 +1,76 @@
+public class Lista implements IList{
+    private int[] array;
+    private int size;
+
+    public Lista(){
+        this.array = new int[2];
+        this.size = 0;
+    }
+
+    @Override
+    public int get(int index) {
+        if(index>=this.size || index<0){
+          throw new RuntimeException("invalid index");
+        }
+        return this.array[index];
+
+    }
+
+    @Override
+    public void add(int element) {
+        if(this.size>=array.length){
+         resize();
+        }
+      this.array[size]=element;
+      this.size++;
+    }
+
+    @Override
+    public void add(int element, int index) {
+        if(!(index>=0 && index<size)){
+            throw new RuntimeException("invalid index");
+        }
+        if(this.size>=array.length){
+            resize();
+        }
+        for(int i = this.size-1;i>=index;i--){
+            this.array[i+1]=this.array[i];
+        }
+        this.array[index]=element;
+        this.size++;
+    }
+
+    @Override
+    public void remove(int index) {
+        if(!(index>=0 && index<size)){
+            throw new RuntimeException("invalid index");
+        }
+        for(int i = index; i<size-1;i++){
+            array[i]=array[i+1];
+        }
+        this.size--;
+        array[size]=0;
+    }
+
+    @Override
+    public void replace(int element, int index) {
+        if(!(index>=0 && index<size)){
+            throw new RuntimeException("invalid index");
+        }
+        array[index]=element;
+    }
+
+    @Override
+    public int getSize() {
+        return this.size;
+    }
+
+    private void resize(){
+        int[] arraytemp = array;
+        array = new int[arraytemp.length*2];
+        for(int i = 0; i<arraytemp.length;i++){
+        array[i] = arraytemp[i];
+    }
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        IList lista1 = new Lista();
+        lista1.add(4);
+        lista1.add(8);
+        lista1.add(12);
+        lista1.add(9,0);
+}}
